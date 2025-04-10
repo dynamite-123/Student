d51f7df6b5f7f70983640f1efe2ebcb7a44e45fc
yashasurs <yashasmanjuurs@gmail.com>
2025-04-10 12:34:26 +0530

    app setup

--- backend/content_generation/content_generation.py
+++ backend/content_generation/content_generation.py
@@ -12,7 +12,7 @@ load_dotenv()
 # os.environ["GEMINI_API_KEY"] = os.getenv["GEMINI_API_KEY"]
 
 # Configure Gemini API
-genai.configure(api_key="AIzaSyAYgz6Eua8KDZ8E08w-4SAtkKuAq_GHyJM")
+genai.configure(api_key="AIzaSyB3UPPzbET8P03O_O55ArY4TKIamnSr9-k")
 
 
 # Define Pydantic models for structured output
