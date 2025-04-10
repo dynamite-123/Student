895ed7a7e40bcb6e97fd7c5cb0ea7762eb66b943
yashasurs <yashasmanjuurs@gmail.com>
2025-04-10 15:20:13 +0530

    homepage

--- Appdev/student/app/(tabs)/index.tsx
+++ Appdev/student/app/(tabs)/index.tsx
@@ -1,9 +1,42 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import SearchBar from "@/app/components/SearchBar";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View className="flex-1 items-center justify-center bg-gray-100">
-      <Text className="text-5xl text-dark-100 font-bold">Welcome to my application</Text>
+    <View style={{ flex: 1, backgroundColor: "#000000" }}>
+      <Image
+        source={images.bg}
+        style={{ position: "absolute", width: "100%", zIndex: 0 }}
+        resizeMode="cover"
+      />
+      <ScrollView
+        style={{ flex: 1, paddingHorizontal: 20 }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image
+          source={icons.logo}
+          style={{
+            width: 48,
+            height: 40,
+            marginTop: 80,
+            marginBottom: 20,
+            alignSelf: "center",
+          }}
+        />
+        <View style={{ flex: 1, marginTop: 20 }}>
+          <SearchBar
+            onPress={() => {
+              router.push("/search");
+            }}
+            placeholder="Search for any topic"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
